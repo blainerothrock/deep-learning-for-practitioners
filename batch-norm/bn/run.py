e73311b28ec1a939c602318f47b125a4698c808b
Grant Gasser <glgasser@gmail.com>
2020-05-01 22:10:38 -0700

    Update plots.

--- batch-norm/bn/run.py
+++ batch-norm/bn/run.py
@@ -9,30 +9,30 @@ _, _, _, _, _, _ = train(model_type=ModelType.SIMPLE_FF_BN_NOISE, num_epochs=10)
 """
 Move plots to another file if they get in the way
 """
-# # plot layer 1
-# plt.plot(l1_mean, 'g', label='%s layer 1 input mean' % model_type)
-# plt.plot(l1_mean_bn, 'b', label='%s layer 1 input mean' % model_type_bn)
-# plt.title('Layer 1 Mean: %s vs. %s' % (model_type, model_type_bn))
-# plt.ylabel('Mean Value Before Activation')
-# plt.xlabel('Iteration')
-# plt.legend(loc='upper left')
-# plt.savefig('imgs/l1_mean.png')
-# plt.close()
-#
-# # plot layer 2
-# plt.plot(l2_mean, 'g', label='%s layer 2 input mean' % model_type)
-# plt.plot(l2_mean_bn, 'b', label='%s layer 2 input mean' % model_type_bn)
-# plt.title('Layer 2 Mean: %s vs. %s' % (model_type, model_type_bn))
-# plt.ylabel('Mean Value Before Activation')
-# plt.xlabel('Iteration')
-# plt.legend(loc='upper left')
-# plt.savefig('imgs/l2_mean.png')
-# plt.close()
-#
-# # plot model loss
-# plt.plot(loss_arr, 'r', label='%s loss' % model_type)
-# plt.plot(loss_arr_bn, 'y', label='%s loss' % model_type_bn)
-# plt.title('Loss: %s vs. %s' % (model_type, model_type_bn))
-# plt.legend(loc='upper right')
-# plt.savefig('imgs/loss.png')
-# plt.close()
+# plot layer 1
+plt.plot(l1_mean, 'g', label='%s layer 1 input mean' % model_type)
+plt.plot(l1_mean_bn, 'b', label='%s layer 1 input mean' % model_type_bn)
+plt.title('Layer 1 Mean: %s vs. %s' % (model_type, model_type_bn))
+plt.ylabel('Mean Value Before Activation')
+plt.xlabel('Iteration')
+plt.legend(loc='upper left')
+plt.savefig('imgs/l1_mean.png')
+plt.close()
+
+# plot layer 2
+plt.plot(l2_mean, 'g', label='%s layer 2 input mean' % model_type)
+plt.plot(l2_mean_bn, 'b', label='%s layer 2 input mean' % model_type_bn)
+plt.title('Layer 2 Mean: %s vs. %s' % (model_type, model_type_bn))
+plt.ylabel('Mean Value Before Activation')
+plt.xlabel('Iteration')
+plt.legend(loc='upper left')
+plt.savefig('imgs/l2_mean.png')
+plt.close()
+
+# plot model loss
+plt.plot(loss_arr, 'b', label='%s loss' % model_type)
+plt.plot(loss_arr_bn, 'g', label='%s loss' % model_type_bn)
+plt.title('Loss: %s vs. %s' % (model_type, model_type_bn))
+plt.legend(loc='upper right')
+plt.savefig('imgs/loss.png')
+plt.close()
