f0f1aa367aefed5346dd28ef6d197a229116ec99
Grant Gasser <glgasser@gmail.com>
2020-05-05 15:43:33 -0700

    Perform learning rate test.

--- batch-norm/bn/run_linear.py
+++ batch-norm/bn/run_linear.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 
 from bn.train_linear import train, ModelType
 
-l1_mean, l1_std, l2_mean, l2_std, loss_arr, model_type = train(model_type=ModelType.LINEAR, num_epochs=10)
-l1_mean_bn, l1_std_bn, l2_mean_bn, l2_std_bn, loss_arr_bn, model_type_bn = train(model_type=ModelType.LINEAR_BN, num_epochs=10)
-_, _, _, _, _, _ = train(model_type=ModelType.LINEAR_BN_NOISE, num_epochs=10)
+l1_mean, l1_std, l2_mean, l2_std, loss_arr, model_type = train(model_type=ModelType.LINEAR, num_epochs=2)
+l1_mean_bn, l1_std_bn, l2_mean_bn, l2_std_bn, loss_arr_bn, model_type_bn = train(model_type=ModelType.LINEAR_BN, num_epochs=2)
+#_, _, _, _, _, _ = train(model_type=ModelType.LINEAR_BN_NOISE, num_epochs=10)
 
 """
 Move plots to another file if they get in the way
@@ -36,3 +36,11 @@ Move plots to another file if they get in the way
 # plt.legend(loc='upper right')
 # plt.savefig('imgs/loss.png')
 # plt.close()
+
+# plot for to see learning rate difference
+plt.plot(loss_arr, 'y', label='%s loss' % model_type)
+plt.plot(loss_arr_bn, 'r', label='%s loss' % model_type_bn)
+plt.title('Loss: %s vs. %s' % (model_type, model_type_bn))
+plt.legend(loc='upper right')
+plt.savefig('imgs/loss.png')
+plt.close()
